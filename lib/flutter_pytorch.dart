43e5d2eefe1703b05df0f677bc395fdbf48846c6
Geovani <geovanimendoza.h@gmail.com>
2020-08-08 23:46:34 -0400

    style: format main class

--- lib/flutter_pytorch.dart
+++ lib/flutter_pytorch.dart
@@ -7,7 +7,7 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-class FlutterPytorch {
+class PyTorch {
   static const MethodChannel _channel =
       const MethodChannel('flutter_pytorch');
 
--- test/flutter_pytorch_test.dart
+++ test/flutter_pytorch_test.dart
@@ -18,6 +18,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await FlutterPytorch.platformVersion, '42');
+    expect(await PyTorch.platformVersion, '42');
   });
 }
